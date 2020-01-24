d1b3f6e83381ebd82cf06468707b8683df5e2366
Levente Paplogo <lev9433@gmail.com>
2020-01-24 19:21:40 +0100

    it works now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include "SerialCommunication.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SerialCommunication* serial = new SerialCommunication("\\\\.\\COM12", 115200);
+    char recievedPacketAngle[10];
+    char recievedPacketDistance[10];
+    serial->send("s s\n", 5);
+    serial->send("s s\n", 5);
+    serial->send("s s\n", 5);
+    serial->send("s s\n", 5);
+    serial->send("s s\n", 5);
+    serial->send("s s\n", 5);
+    serial->send("s s\n", 5);
+    serial->send("s s\n", 5);
+    for (int i = 0; i < 10; i++) {
+        recievedPacketAngle[i] = '\0';
+        recievedPacketDistance[i] = '\0';
+    }
+    while (1) {
+        serial->read(recievedPacketAngle, recievedPacketDistance,10);
+        std::cout << "angle:";
+        printf("%s", recievedPacketAngle);
+        std::cout << "distance:";
+        printf("%s", recievedPacketDistance);
+        std::cout << std::endl;
+        for (int i = 0; i < 10; i++) {
+            recievedPacketAngle[i] = '\0';
+        }
+        for (int i = 0; i < 10; i++) {
+            recievedPacketDistance[i] = '\0';
+        }
+    }
     return 0;
 }
