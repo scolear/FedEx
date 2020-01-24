85908d816d227e2878de88f4faf0aca8ea938a05
hrumocsaba <hrumos3@gmail.com>
2020-01-24 22:23:34 +0100

    perfected refresh and refactored some functions, convert function need to be used

--- main.cpp
+++ main.cpp
@@ -5,25 +5,15 @@ int main() {
     std::srand(time(NULL));
     int width = 600;
     DataToMat dataverter(width);
-    std::vector<float> data;
-    
-    while (cv::waitKey(27))
+    cv::Mat img(width, width, CV_8UC3, cv::Scalar(0, 0, 0));
+    while (1)
     {
-        cv::Mat img(width, width, CV_8UC3, cv::Scalar(0, 0, 0));
-        for (float i = 0; i < 360; i++)
+        for (int i = 0; i < 360; i++)
         {
-            //data.push_back((rand() % (width / 2)));
-            int random = (rand() % (width / 2));
-            dataverter.drawColoredPoints(img, i,  random);
-            //data.push_back(50);
-            cv::imshow("test", img);
+            int random = (rand() % (width / 2)+20);            
+            cv::imshow("test", dataverter.convert(img, i, random));
             cv::waitKey(1);
-
         }
-        //dataverter.dottedShow(img, data);
-        //data.clear();
-        img.release();
-        
     }
     
     return 0;
